2f437e845453860cf4d177f213cd724c73a5ae8e
Joe Sturzenegger <joesturz84@gmail.com>
2024-09-22 21:50:22 -0600

    get list of locationTypes

--- mobile/MapExercise/MapExercise/ContentView.swift
+++ mobile/MapExercise/MapExercise/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
         center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
+    //TODO: Add filter on location types
+    @State private var locationTypes: Set<String> = []
     
     var body: some View {
         let bounds = MapCameraBounds(centerCoordinateBounds: region)
@@ -22,7 +24,7 @@ struct ContentView: View {
                 let name = location.getName()
                 Marker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
                     Text(name)
-                }
+                }.tag(location.id)
             }
         }.onAppear {
             loadLocations()
@@ -39,8 +41,16 @@ struct ContentView: View {
             let data = try Data(contentsOf: url)
             let decoder = JSONDecoder()
             locations = try decoder.decode([Location].self, from: data)
+            loadLocationTypes()
         } catch {
             print("Failed to load or decode data: \(error)")
+        }
+    }
+    
+    func loadLocationTypes() {
+        locationTypes.insert("all")
+        for location in locations {
+            locationTypes.insert(location.getLocationType())
         }
     }
 }
